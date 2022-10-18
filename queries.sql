301bada4291a22f6f9a2b2cd2ce88b17e5fe9e88
Bakhtiyor <harhil46@gmail.com>
2022-10-18 21:59:14 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,26 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals
+WHERE name LIKE '%mon';
+
+select (name) from animals
+WHERE date_of_birth between '2016-01-01' and '2019-01-01';
+
+select (name) from animals
+WHERE neutered = true 
+and escape_attempts < 3;
+
+select name, escape_attempts from animals
+WHERE weight_kg > 10.5;
+
+select date_of_birth from animals
+WHERE name in ('Agumon', 'Pikachu');
+
+select * from animals
+WHERE neutered = true;
+
+select * from animals
+WHERE name not in ('Gabumon');
+
+select * from animals
+WHERE weight_kg between 11.3 and 17.3;
