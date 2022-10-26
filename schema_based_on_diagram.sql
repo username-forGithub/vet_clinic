ef9813ac1d50630a1dfe5d59d7c76f1ab9a347b4
Bakhtiyor <harhil46@gmail.com>
2022-10-26 21:47:29 +0500

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -49,8 +49,9 @@ CREATE TABLE medical_histories_join_treatments(
     CONSTRAINT treatments_id_fkey FOREIGN KEY(treatment_id) REFERENCES treatments(id),
 )
 
-CREATE INDEX invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX invoice_items_invoice_id ON invoices_items(invoice_id);
 CREATE INDEX medical_histories_patient_id ON medical_histories(patient_id);
 CREATE INDEX medical_histories_join_treatments_histories_id ON medical_histories_join_treatments(histories_id);
 CREATE INDEX medical_histories_join_treatments_treatment_id ON medical_histories_join_treatments(treatment_id);
 CREATE INDEX invoices_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX invoice_items_treatment_id ON invoices_items(treatment_id);
